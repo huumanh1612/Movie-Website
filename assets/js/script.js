33034d9be5a932b16e6a84ee070389cb34217bfd
WINDOWS-F6D8G21\Thông <21522644@gm.uit.edu.vn>
2024-04-24 22:06:23 +0700

    Add Log-Regis.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,5 @@
 'use strict';
-// Log in - Sign up
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
 
-registerLink.addEventListener('click', ()=> {
-  wrapper.classList.add('active');
-});
-
-loginLink.addEventListener('click', ()=> {
-  wrapper.classList.remove('active');
-});
 
 /**
  * navbar variables
